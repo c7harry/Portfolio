185ab0f8530a728e5084d3e88c9d116c411f8e6b
c7harry <76096139+c7harry@users.noreply.github.com>
2025-05-17 23:16:29 -0700

    fixed hosting

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -40,7 +40,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fa58683668ee968b863ab5cac03c7f79
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "420da43f9d79f263f85ba91c27e319e9",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ca1cd9175980d364c931e76d1c828de7",
+"flutter_bootstrap.js": "0474a0b8313c5757f7649db595544d7f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
