e144948ac3b4e2a7b7e328ca1049afbd11d65e33
c7harry <76096139+c7harry@users.noreply.github.com>
2025-06-02 01:33:17 -0700

    fixed overflow

--- lib/pages/header.dart
+++ lib/pages/header.dart
@@ -4,19 +4,30 @@ class HeaderSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
-    final backgroundColor = isDark
-        ? const Color.fromARGB(255, 9, 10, 9)
-        : Colors.white;
+    final backgroundColor =
+        isDark ? const Color.fromARGB(255, 9, 10, 9) : Colors.white;
 
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 24),
       width: double.infinity,
+      padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 24),
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [backgroundColor, backgroundColor],
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
         ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min, 
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Your Name", style: Theme.of(context).textTheme.headlineMedium),
+          const SizedBox(height: 16),
+          Text(
+            "Your Tagline or Title",
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ],
       ),
     );
   }
